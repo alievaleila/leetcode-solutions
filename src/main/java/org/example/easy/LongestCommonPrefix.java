feefc30f9ab9b31176b31b00abb1d9266326efcc
alievaleila <meliyeva452@gmail.com>
2026-02-19 09:33:14 +0400

    solve: LeetCode 14 - Longest Common Prefix (Java)

--- src/main/java/org/example/easy/LongestCommonPrefix.java
+++ src/main/java/org/example/easy/LongestCommonPrefix.java
@@ -2,7 +2,27 @@ package org.example.easy;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-
+        System.out.println(longestCommonPrefix(new String[]{"Salam","Sal"}));
     }
 
+    public static String longestCommonPrefix(String[] strs){
+
+        if(strs==null||strs.length==0){
+            return "";
+        }
+
+        String prefix=strs[0];
+
+        for (int i = 0; i < strs.length; i++) {
+
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+
+            if (prefix.isEmpty()) {
+                return "";
+            }
+        }
+        return prefix;
+    }
 }
